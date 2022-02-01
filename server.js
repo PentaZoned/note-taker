aa1ec134ec82f6c2e8f061e2bd16a2755d9c5679
Bradley Le <brabrele@gmail.com>
2022-01-31 16:29:38 -0800

    added GET routes to homepage and the notes page, added port listener, allowed static pages to be served

--- server.js
+++ server.js
@@ -1,6 +1,26 @@
+// imported and required packages
 var express = require('express');
+const path = require('path');
 
-var app = express();
+const PORT = 3000;
 
-var PORT = 3000;
+// Creates an express application
+const app = express();
 
+// Allows us to serve static files such as index.html and notes.html
+app.use(express.static('public'));
+
+// GET route for homepage
+// Sets the current webpage to index.html upon visit
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET route for the notes page
+// Redirects from the home page to notes.html
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// listener for the port
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
