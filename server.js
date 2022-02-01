a8dc18b645990d469c0fd601c34b6504d767fac1
Bradley Le <brabrele@gmail.com>
2022-01-31 19:10:40 -0800

    added fs and util packages, also created read, write, and append functions

--- server.js
+++ server.js
@@ -1,14 +1,44 @@
 // imported and required packages
 var express = require('express');
 const path = require('path');
+const fs = require('fs');
+const util = require('util');
 
 const PORT = process.env.PORT || 3000;
 
 // Creates an express application
 const app = express();
 
+// Middleware
+// a method to recognize incoming request objects as JSON objects, 
+app.use(express.json());
+// a method to recognize incoming request objects as strings or arrays
+app.use(express.urlencoded({ extended: true }));
 // Allows us to serve static files such as index.html and notes.html
 app.use(express.static('public'));
+
+// Allows the JSON file to be readable
+const readFromFile = util.promisify(fs.readFile);
+
+// function that writes to a specific location with the contents
+const writeToFile = (destination, content) =>
+    fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.log(err) : console.log(`Data writtne to ${destination}`)
+    );
+
+// Reads a file and appends data
+const readAndAppend = (content, file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+        if(err) {
+            console.log(err);
+        } else {
+            const parsedData = JSON.parse(data);
+            parsedData.push(content);
+            writeToFile(file, parsedData);
+        }
+    });
+};
+
 
 // GET route for homepage
 // Sets the current webpage to index.html upon visit
@@ -21,6 +51,12 @@ app.get('/', (req, res) =>
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+
+app.get('/api/notes', (req, res) => {
+    res.json('db/db.json');
+});
+
+
 
 // listener for the port
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
